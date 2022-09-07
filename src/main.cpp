f67dc94be376b82d554a80089e06776ad6da84dc
ThatColdToast <72617136+ThatColdToast@users.noreply.github.com>
2022-09-06 20:36:27 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "vendor/stb/stb_image.h"
-
-#define STB_VOXEL_RENDER_IMPLEMENTATION
-#define STBVOX_CONFIG_MODE 0
-#include "vendor/stb/stb_voxel_render.h"
-
 int main()
 {
     std::cout << "Hello World!" << std::endl;
